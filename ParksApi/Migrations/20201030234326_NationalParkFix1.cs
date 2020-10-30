3e843179ded50c492bd06f912ac9c11b95b67991
3Emme <emmettbuentiempo@gmail.com>
2020-10-30 16:51:29 -0700

    complete migration to fix NationalPark model

---
+++ ParksApi/Migrations/20201030234326_NationalParkFix1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksApi.Migrations
+{
+    public partial class NationalParkFix1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NationalParkDescription",
+                table: "NationalParks",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NationalParkDescription",
+                table: "NationalParks");
+        }
+    }
+}
